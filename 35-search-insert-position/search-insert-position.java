fc7d914706950730bf64db21c10daf80cbb1e8ae
Jodhi <jodhiparson@yahoo.com>
2024-01-25 15:29:32 -0500

    Time: 0 ms (100.00%) | Memory: 42.8 MB (81.46%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,17 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        if (nums == null) {
-            return -1;
-        }
-        int i = 0;
-        while (i < nums.length) {
-            if (nums[i] == target || nums[i] > target) {
-                return i;
-             } else {
-                i++;
+        int left = 0;
+        int right = nums.length-1;
+        while (left <= right) {
+            int mid = left + (right-left)/2; //midpoint
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid -1;
             }
         }
-        return i;
+        return left;
             }
         }
     
